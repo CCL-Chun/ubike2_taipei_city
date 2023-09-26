ac5f6cdc0c05efcdbd3d741c815def5fd00dacb4
Kevin Lee <56715642+CCL-Chun@users.noreply.github.com>
2023-09-27 00:35:02 +0800

    Update catch_taipeicity_ubike2_per_min.py
    add description

--- scripts/catch_taipeicity_ubike2_per_min.py
+++ scripts/catch_taipeicity_ubike2_per_min.py
@@ -5,35 +5,33 @@ from datetime import datetime
 import os
 
 # Set API URLs
-url = "https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json"
+url = "https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json" #taipei city
 data_path = "./"
 
+# Function to get data
 def fetch_data_from_url(url):
     while True:
         try:
-            response = requests.get(url, timeout=10)
-            response.raise_for_status()
+            response = requests.get(url, timeout=10) #try to get response in 10s
+            response.raise_for_status() #raise HTTPError if no response
             return response.json()
-        except requests.RequestException as e:
+        except requests.RequestException as e: #show error code to log
             print(f"Error fetching data from {url}. Retrying... Error: {e}")
-            time.sleep(5)
+            time.sleep(5) #retry after 5s
 
+# Function to transform data and save the table
 def get_and_save_data():
-    # Fetch data from both URLs with retry mechanism
     data = fetch_data_from_url(url)
-    
-    # Combine data from both URLs
-    all_data = data
 
-    # Convert collected data to DataFrame
-    df = pd.DataFrame(all_data)
-    timestamp = datetime.now().strftime('%Y-%m-%d_%H:%M:%S')
-    file_path = os.path.join(data_path, f"data_{timestamp}.txt")
+    #convert collected data (JSON) to DataFrame
+    df = pd.DataFrame(data)
+    timestamp = datetime.now().strftime('%Y-%m-%d_%H:%M:%S') #time format
+    file_path = os.path.join(data_path, f"data_{timestamp}.txt") #set file name with a time stamp
 
-    # Save DataFrame to .txt
+    #save DataFrame to .txt
     df.to_csv(file_path, sep='\t', index=False)
 
-    print(f"Data saved at: {file_path} \t nrow={len(df)}")
+    print(f"Data saved at: {file_path} \t nrow={len(df)}") #write a log info for each saving process
 
 # Infinite loop to run every 60 seconds
 while True:
